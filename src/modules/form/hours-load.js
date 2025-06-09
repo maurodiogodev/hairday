551f064d25cc2452d6dc1c7e6944c65bf0c1a3bb
Mauro Diogo <mauromjdiogo@gmail.com>
2025-06-09 05:48:47 -0300

    load available hours first settings

---
+++ src/modules/form/hours-load.js
@@ -0,0 +1,17 @@
+import dayjs from "dayjs"
+import { openingHours } from "../../utils/opening-hours.js"
+
+export function hoursLoad({ date }) {
+    const opening = openingHours.map((hour) => {
+        const [scheduleHour, _] = hour.split(":")
+
+        const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+
+        return {
+            hour,
+            available: isHourPast
+        }
+    })
+
+    console.log(opening)
+}
