ce433fa0d9fcd4cac0ec6035204e86f4c9a012a7
Mauro Diogo <mauromjdiogo@gmail.com>
2025-06-10 04:55:35 -0300

    listing available schedule

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/opening-hours.js"
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad({ date }) {
     const opening = openingHours.map((hour) => {
         const [scheduleHour, _] = hour.split(":")
@@ -13,5 +15,14 @@ export function hoursLoad({ date }) {
         }
     })
 
-    console.log(opening)
+    opening.forEach(({ hour, available }) => {
+        const li = document.createElement("li")
+
+        li.classList.add("hour")
+        li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+        li.textContent = hour
+
+        hours.append(li)
+    })
 }
