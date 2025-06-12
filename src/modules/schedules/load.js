1cb51e59f2c32df36ff7a82d219439611564c50f
Mauro Diogo <mauromjdiogo@gmail.com>
2025-06-12 05:35:30 -0300

    getting datas from the API

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,13 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
 
 const selectedDate = document.getElementById("date")
 
-export function schedulesDay() {
+export async function schedulesDay() {
     const date = selectedDate.value
+
+    const dailySchedules = await scheduleFetchByDay({ date })
+    console.log(dailySchedules)
 
     hoursLoad({ date })
 }
