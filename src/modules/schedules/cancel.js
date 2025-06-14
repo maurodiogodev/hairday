7d93c80f5826017cd3b6a019d387090283ba895f
Mauro Diogo <mauromjdiogo@gmail.com>
2025-06-14 09:34:40 -0300

    canceling schedule feature

---
+++ src/modules/schedules/cancel.js
@@ -0,0 +1,23 @@
+import { schedulesDay } from "./load.js"
+import { scheduleCancel } from "../../services/schedule-cancel.js"
+
+const periods = document.querySelectorAll(".period")
+
+periods.forEach((period) => {
+    period.addEventListener("click", async (event) => {
+        if (event.target.classList.contains("cancel-icon")) {
+            const item = event.target.closest("li")
+            const { id } = item.dataset
+
+            if (id) {
+                const isConfirm = confirm("Tem certeza que deseja cancelar esse agendamento?")
+
+                if (isConfirm) {
+                    console.log("remover")
+                    await scheduleCancel({ id })
+                    schedulesDay()
+                }
+            }
+        }
+    })
+})
